c4d370c8d30f986674b603077e9fccdc2c3c3fb8
jrfep <jr.ferrer.paris@gmail.com>
2017-06-12 08:51:27 -0400

    descargar lista de archivos Modis

---
+++ R/inc00_ImportarModisLST.R
@@ -0,0 +1,51 @@
+##R --vanilla
+##
+require(rvest)
+setwd("~/tmp")
+## Para la representación de la temperatura de la superficie de la tierra utilizamos el producto MOD11A2 derivado de datos de sensores Modis.
+
+## Información sobre la fuente de datos y sus características espaciales y temporales se encuentra en:
+## #https://gcmd.nasa.gov/KeywordSearch/Metadata.do?Portal=daacs&KeywordPath=Parameters|LAND+SURFACE|SURFACE+THERMAL+PROPERTIES|LAND+SURFACE+TEMPERATURE|[Freetext%3D%27Modis%27]&OrigMetadataNode=GCMD&EntryId=MOD11A25&MetadataView=Full&MetadataType=0&lbnode=mdlb1
+
+## datos sobre la validación del producto
+## #https://landval.gsfc.nasa.gov/ProductStatus.php?ProductID=MOD11
+
+## Aceso directo a los datos:
+## #https://e4ftl01.cr.usgs.gov/MOLT/
+
+repositorio <- "https://e4ftl01.cr.usgs.gov/MOLT/"
+
+## existen varias versiones disponibles, la version 6 es más reciente, pero no se ha actualizado la información sobre su validación, por tanto usamos la versión 005
+## # MOD11A2.004/ 
+## # MOD11A2.005/ 
+## # MOD11A2.006/      
+
+versiones <- "MOD11A2.005/"
+
+## Definimos el directorio para descargar los datos:
+mapoteca <- "~/mapas/download/MOD11A2/V5/hdfs"
+system(sprintf("mkdir -p %s",mapoteca))
+
+## descargamos la lista de directorios (fechas de la serie de tiempo)
+download.file(sprintf("%s%s",repositorio,versiones),
+              sprintf("%s/indice.html",mapoteca))
+
+## leemos esta lista:
+pg <- read_html(sprintf("%s/indice.html",mapoteca))
+lista.fechas <- grep("^2[0-9]+",lapply(html_nodes(pg,"a"),xml_text),value=T)
+
+## para cada fecha descargamos la lista de archivos, y extraemos los archivos que cubren Venezuela, definidos por las files (h11 y h12) y columnas (v07 y v08).
+
+##aa <- lista.fechas[1]
+for (aa in lista.fechas) {
+    download.file(sprintf("%s%s%s",repositorio,versiones,aa),
+                  sprintf("%s/fecha.html",mapoteca))
+    
+    pg1 <- read_html(sprintf("%s/fecha.html",mapoteca))
+    hdfs <- unlist(lapply(html_nodes(pg1,"a"),xml_attr,"href"))
+    
+    cat(file=sprintf("%s/lista_archivos_descarga",mapoteca),
+        sprintf("%s%s%s%s\n",repositorio,versiones,aa,
+                grep("hdf",grep("h1[12]v0[78]",hdfs,value=T),value=T)),
+        append=T)
+}
