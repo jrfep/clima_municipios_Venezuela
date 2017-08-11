71533e3904802a827742a5b9c90efeaa2dd33627
jrfep <jr.ferrer.paris@gmail.com>
2017-08-11 08:18:36 -0400

    revisando datos de precipitación descargados por estado (2000 a 2017)

--- R/inc00_ImportarDatosINAMEH.R
+++ R/inc00_ImportarDatosINAMEH.R
@@ -1,11 +1,11 @@
-##R --vanilla
-
-## Los datos originales de las estaciones climáticas de Zulia, Bolívar, Lara y Falcón fueron descargados en octubre 2015 de la página del Instituto Nacional de Meteorología e Hidrología de la República Bolivariana de Venezuela (INAMEH, http://www.inameh.gob.ve/web/, enlace de "Climatología" / "Datos Hidrometeorológicos"). En mayo de 2017 este enlace está roto (mensaje "No support") y no se pudo completar la descarga de datos de otros estados
+## Los datos originales de las estaciones climáticas de Zulia, Bolívar, Lara y Falcón fueron descargados en octubre 2015 de la página del Instituto Nacional de Meteorología e Hidrología de la República Bolivariana de Venezuela (INAMEH, http://www.inameh.gob.ve/web/, enlace de "Climatología" / "Datos Hidrometeorológicos"). En mayo de 2017 este enlace está roto (mensaje "No support") y no se pudo completar la descarga de datos de otros estados.
 
 ## Para Agosto de 2017 los datos estaban disponibles nuevamente a través de un formulario web ubicado en http://estaciones.inameh.gob.ve/descargaDatos/vistas/bajarArchivo.php
-## Aparentemente, a través de este formulario se pueden descargar datos recientes (2015 a 2017) de todos los estados.
+## Aparentemente, a través de este formulario se pueden descargar datos recientes de todos los estados, pero existen notables vacíos en la cobertura temporal. Para la mayoría de los estados hay datos de 2000 a 2017 pero con grandes vacíos entre 2006 y 2012.
+## El procedimiento para descargar estos datos desde una sesión de 'R' y con ayuda del programa 'wget' se detalla a continuación:
 
-## El procedimiento para descargar estos datos es el siguiente:
+##R --vanilla
+require(gdata)
 
 ## definimos la carpeta destino para la descarga:
 dir.datos <- "../data/INAMEH"
@@ -22,8 +22,8 @@ for (vv in c(1600,1610,1620,3800)) {
     ## lista de los IDs para los estados
     for (edos in 1:24) {
         ## definimos los años de interés
-        ##(aparentemente solo disponible entre 2015 y 2017)
-        for (yy in 2010:2017) {
+        ##(aparentemente solo disponible entre 2000 y 2017, con vacíos)
+        for (yy in 2000:2017) {
             ## construimos una solicitud de descarga que enviamos con el
             ## programa 'wget' usando la opción 'post-data' y grabamos a un
             ## archivo en la carpeta seleccionada, con el año y código de
@@ -38,3 +38,27 @@ for (vv in c(1600,1610,1620,3800)) {
 ## '77777.7: Dato Faltante.'
 ## '66666.6: Dato Erroneo.'
 ## '99999.9: Dato Perdido.'
+
+## También aparecen valores de 88888.8 en las series de tiempo, pero no se explica a que se refiere este código.
+
+
+## Resumimos todos los datos de precipitación
+
+dts <- data.frame()
+for (edos in 1:24) {
+    for (yy in 2017:1990) {
+        dd <- try(read.xls(sprintf("%s/var3800/%s_%s.xls",dir.datos,yy,edos),stringsAsFactors=F))
+        if (!any(class(dd) %in% "try-error"))
+            dts <- rbind(dts,dd)
+    }
+}
+dts <- subset(dts,!valor %in% c(66666.6,77777.7,88888.8,99999.9))
+dts$fch <- sapply(as.character(dts$Fecha),function(x) as.numeric(strsplit(x,"-")[[1]][1])+((as.numeric(strsplit(x,"-")[[1]][2])-0.5)/12))
+
+## Gráfica de valores de precipitación por fecha
+plot(valor~fch,dts)
+
+##para ver la distribución de registros por año
+table(floor(dts$fch))
+
+
